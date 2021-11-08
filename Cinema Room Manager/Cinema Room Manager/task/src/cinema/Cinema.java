1e72de712838259ee940967ac259da816ecf73fb
Kidchai <o.naibova@gmail.com>
2021-11-07 19:53:55 -0600

    Added the calculation of the profit from all the sold tickets depending on the number of available seats

--- Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,16 +1,28 @@
 package cinema;
+import java.util.Scanner;
 
 public class Cinema {
 
     public static void main(String[] args) {
-        System.out.println("Cinema:");
-        System.out.println("  1 2 3 4 5 6 7 8");
-        System.out.println("1 S S S S S S S S");
-        System.out.println("2 S S S S S S S S");
-        System.out.println("3 S S S S S S S S");
-        System.out.println("4 S S S S S S S S");
-        System.out.println("5 S S S S S S S S");
-        System.out.println("6 S S S S S S S S");
-        System.out.println("7 S S S S S S S S");
+        System.out.println("Enter the number of rows:");
+        Scanner scanner = new Scanner(System.in);
+        int numberOfRows = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int numberOfSeatsInRow = scanner.nextInt();
+        int totalIncome = 0;
+        if (numberOfRows * numberOfSeatsInRow <= 60) {
+            totalIncome = numberOfRows * numberOfSeatsInRow * 10;
+        } else {
+            if (numberOfRows % 2 == 0) {
+                int frontAndBackRows = numberOfRows / 2;
+                totalIncome = (frontAndBackRows * numberOfSeatsInRow * 10) + (frontAndBackRows * numberOfSeatsInRow * 8);
+            } else {
+                int frontRows = numberOfRows / 2;
+                int backRows = numberOfRows / 2 + 1;
+                totalIncome = (frontRows * numberOfSeatsInRow * 10) + (backRows *numberOfSeatsInRow * 8);
+            }
+        }
+        System.out.println("Total income:");
+        System.out.println("$" + totalIncome);
     }
 }

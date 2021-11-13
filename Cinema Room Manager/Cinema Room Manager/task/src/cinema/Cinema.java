5d0d5819fc48d0d1da0f730864789ec1dcd6a744
Kidchai <o.naibova@gmail.com>
2021-11-12 21:44:19 -0600

    1. The option to view statistics was added to the menu 2. The exception handling was added in case the user try to buy a non-existent seat 3. Now the user can't buy an already bought seat

--- Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 
 public class Cinema {
     static boolean shouldWork = true;
+    static int purchasedTickets = 0;
+    static int currentIncome = 0;
+    static int numberOfRows = 0;
+    static int numberOfSeatsInRow = 0;
 
     public static void main(String[] args) {
+        int[] ticketInfo = new int[2];
         System.out.println("Enter the number of rows:");
         Scanner scanner = new Scanner(System.in);
-        int numberOfRows = scanner.nextInt();
+        numberOfRows = scanner.nextInt();
         System.out.println("Enter the number of seats in each row:");
-        int numberOfSeatsInRow = scanner.nextInt();
+        numberOfSeatsInRow = scanner.nextInt();
         char[][] array = new char[numberOfRows + 1][numberOfSeatsInRow + 1];
         int REDIX = 10;
         for(int i = 0; i < array.length; i++) {
@@ -28,6 +33,7 @@ public class Cinema {
         while (shouldWork) {
             System.out.println("1. Show the seats\n" +
                     "2. Buy a ticket\n" +
+                    "3. Statistics\n" +
                     "0. Exit");
             int option = scanner.nextInt();
             switch (option) {
@@ -35,7 +41,10 @@ public class Cinema {
                     showSeats(array);
                     break;
                 case 2:
-                    buyTicket(numberOfRows, numberOfSeatsInRow, array);
+                    ticketInfo = buyTicket(array);
+                    break;
+                case 3:
+                    showStatistics(ticketInfo);
                     break;
                 case 0:
                     shouldWork = false;
@@ -52,23 +61,55 @@ public class Cinema {
             for(char i : a) {
                 System.out.print(i + " ");
             }
-            System.out.println("");
+            System.out.print("\n");
         }
     }
 
-    private static void buyTicket(int numberOfRows, int numberOfSeatsInRow, char[][] array) {
+    public static int[] buyTicket(char[][] array) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a row number:");
         int pickedRow = scanner.nextInt();
         System.out.println("Enter a seat number in that row:");
         int pickedSeat = scanner.nextInt();
         int price = 0;
-        if (numberOfRows * numberOfSeatsInRow > 60 && pickedRow > numberOfRows / 2) {
-            price = 8;
-        } else {
-            price = 10;
+        try {
+            if (array[pickedRow][pickedSeat] == 'B') {
+                System.out.println("That ticket has already been purchased!");
+                buyTicket(array);
+            } else {
+                if (numberOfRows * numberOfSeatsInRow > 60 && pickedRow > numberOfRows / 2) {
+                    price = 8;
+                } else {
+                    price = 10;
+                }
+                currentIncome += price;
+                purchasedTickets++;
+                array[pickedRow][pickedSeat] = 'B';
+                System.out.println("Ticket price: $" + price);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Wrong input!");
+            buyTicket(array);
         }
-        array[pickedRow][pickedSeat] = 'B';
-        System.out.println("Ticket price: $" + price);
+        return new int[] {currentIncome, purchasedTickets};
+    }
+
+    private static void showStatistics(int[] ticketInfo) {
+        int totalIncome = 0;
+        if (numberOfRows * numberOfSeatsInRow > 60) {
+            totalIncome = numberOfSeatsInRow * (numberOfRows / 2) * 10 +
+                    (numberOfRows - numberOfRows / 2) * numberOfSeatsInRow * 8;
+        } else {
+            totalIncome = numberOfRows * numberOfSeatsInRow * 10;
+        }
+        float allSeats = numberOfRows * numberOfSeatsInRow;
+        float  purchasedTickets = ticketInfo[1];
+        float percentage = purchasedTickets / allSeats * 100;
+        System.out.printf("Number of purchased tickets: %d\n", ticketInfo[1]);
+        System.out.printf("Percentage: %.2f%%\n", percentage);
+        //System.out.println("Percentage: " + percentage + "%");
+        System.out.printf("Current income: $%d\n", ticketInfo[0]);
+        System.out.printf("Total income: $%d\n", totalIncome);
+        System.out.println("");
     }
 }
